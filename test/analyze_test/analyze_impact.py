9d8a5888584fef3bda22d349b51fb1c26bc118d3
Dawid Maciazek <dawid.maciazek@gmail.com>
2016-08-09 22:03:03 +0200

    bining method tested

--- test/analyze_test/analyze_impact.py
+++ test/analyze_test/analyze_impact.py
@@ -1,10 +1,7 @@
 from simpy import analyze
+import scipy.ndimage as ndimage
 import numpy as np
 import matplotlib.pyplot as plt
-
-iname = "Ag_50.lammpstrj"
-ifile = analyze.Traj(iname)
-frames = ifile.read(4)
 
 
 def test_area(frames):
@@ -38,4 +35,51 @@ def draw_image(frames):
     plt.imshow(img)
     plt.show()
 
-draw_image(frames)
+
+def bining_test(frames, frame_no):
+    coord = frames[frame_no]['coord']
+    coord = coord[coord[:,1] < 4]
+    coord = coord[coord[:,1] > -4]
+
+    tcoord = coord.transpose()
+    x = tcoord[0]
+    z = tcoord[2]
+    #plt.plot(x, z, "o")
+
+    binsize = 0.15
+    offset = 0.1
+    xmin = np.amin(x) - offset
+    xmax = np.amax(x) + offset
+
+    zmin = np.amin(z) - offset
+    zmax = np.amax(z) + offset
+
+    xbin_range = np.arange(xmin, xmax, binsize)
+    xbin_id = np.digitize(x, xbin_range)
+    xbins = len(xbin_range)
+
+    zbin_range = np.arange(zmin, zmax, binsize)
+    zbin_id = np.digitize(z, zbin_range)
+    zbins = len(zbin_range)
+
+    atoms_img = np.zeros((xbins, zbins), dtype=float)
+
+    print xbin_id[:10]
+    print zbin_id[:10]
+
+    for i in xrange(len(x)):
+        atoms_img[xbin_id[i]-1][zbin_id[i]-1] += 1.0
+
+    atoms_img_conv = ndimage.gaussian_filter(atoms_img, sigma=12 , order=0)
+    plt.imshow(atoms_img_conv)
+    plt.show()
+
+
+iname = "Ag_50.lammpstrj"
+ifile = analyze.Traj(iname)
+frames = ifile.read(20)
+
+for i in range(20):
+    bining_test(frames, i)
+#draw_image(frames)
+
