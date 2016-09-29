1a71bb3ab95ab0f12703415d7448d3de592a32b5
dawid <dawid.maciazek@gmail.com>
2016-09-29 17:13:47 +0200

    select function added

--- simpy/tools.py
+++ simpy/tools.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Frame:
     def __init__(self):
         self.record = {}
@@ -26,3 +28,23 @@ class Frame:
     def gen_element(self, element_name='H'):
         atoms = len(self['coord'])
         self.add('element', [element_name]*atoms)
+
+    def select(self, box):
+        """
+        return frame containing only records inside box
+        box=[[xmin, xmax], [ymin, ymax], [zmin, zmax]]
+
+        current:
+            only coordinates
+        """
+
+        coord = self['coord']
+        minCr = np.array([box[0][0], box[1][0], box[2][0]])
+        maxCr = np.array([box[0][1], box[1][1], box[2][1]])
+
+        inside = np.all(np.logical_and(minCr <= coord, coord <= maxCr), axis=1)
+        newCoord = coord[inside]
+
+        oFrame = Frame()
+        oFrame.add('coord', newCoord)
+        return oFrame
